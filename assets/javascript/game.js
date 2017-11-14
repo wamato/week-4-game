751f1215cd08a30f45dd85d24620011e1f2c9c9a
unknown <IamWill216@yahoo.com>
2017-11-14 18:50:19 -0500

    more solid framework

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+var spongebob = {
+    hp= 100,
+    attack= 15,
+    growth= attack+10,
+};
+
+var patrick = {
+    hp= 120,
+    attack= 30,
+    growth= hp+5,
+};
+
+var squidward = {
+    hp=105,
+    attack= 15,
+    growth =attack+5,
+};
+
+var larry = {
+    hp=90,
+    attack= 45,
+    growth= attack+3,
+};
+
+
+$("")
+
+
+
+
+
+
+
+});
